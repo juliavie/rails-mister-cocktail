9db5c1aecc623f16eb5d112afd74a9f8f2568fb8
Julia Viehbaeck <julia.viehbaeck@gmail.com>
2020-11-20 11:09:43 +0100

    added psydo code for API

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,3 +26,18 @@ puts "#{Cocktail.count} cocktail created"
 # 2. open the link
 # 3. loop through the whole data with each
 # 4. for every item in the hash create an ingredient.create
+
+
+# require 'json'
+# require 'open-uri'
+
+# Ingredient.destroy_all
+
+# result = JSON.parse(open('http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list').read)
+# drinks = result['drinks']
+
+# drinks.each do |hash|
+#   hash.each do |k, v|
+#     Ingredient.create(name: v)
+#   end
+# end
