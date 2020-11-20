c25d494c28f34d2ddfc894ed71755791c62238da
Julia Viehbaeck <julia.viehbaeck@gmail.com>
2020-11-20 12:18:04 +0100

    created home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # cocktails
-  root to: "cocktails#index"
+  root to: "pages#home"
   resources :cocktails, only: [:index, :show, :new, :create] do
     # nesting doses
     resources :doses, only: [:new, :create, :destroy]
