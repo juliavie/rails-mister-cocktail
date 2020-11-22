d77d33db984fd51aca65e0687fe39b224fa37033
Julia Viehbaeck <julia.viehbaeck@gmail.com>
2020-11-22 18:36:09 +0100

    improved front-end on home and index page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # cocktails
   root to: "pages#home"
-  resources :cocktails, only: [:index, :show, :new, :create] do
+  resources :cocktails, only: [:index, :show, :new, :create, :destroy] do
     # nesting doses
     resources :doses, only: [:new, :create, :destroy]
   end
